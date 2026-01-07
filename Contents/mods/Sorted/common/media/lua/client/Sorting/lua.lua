a6dfb0c96d130ddb256d5a946a4e9ee58da1375a
Andrzej Pec <andrzej.pec.dev@gmail.com>
2026-01-07 15:58:21 +0100

    Stripping the contents for the initial release

--- Contents/mods/Sorted/common/media/lua/client/Sorting/lua.lua
+++ Contents/mods/Sorted/common/media/lua/client/Sorting/lua.lua
@@ -3,12 +3,12 @@
 -- to jest to rze:
 
 Sorted.Items = {
-    ["Base.ItemNumberOne"] = {
-        [Turbo-original-category] = self:getDisplayCategory() -- mam na myśli że self to "Base.ItemNumberOne" tylko to może zła sygnatura
-        [Shifted-category-if-mapped] = if (self:getDisplayCategory()) -- istnieje jako klucz w mapie no to tu storujemy te kategorie.. przyjkład - self:getDisplayCategory() == VehicleMaintentance, a wiec takie mapowanie isstnieje i zatem nadajemy mu prawda "Mechanics" ale chjcemy to mieć prawda w rekordach jako drugi wpis
-        [Category-Chosen-By-Our-Algorthm] = --czośtam czo wynikło z naaszego sotrotwania np. kurwa Base.TeddyBear -> "Plushie", albo mechanics -> waeapon , no to tutaj pomimo że mapa wskazala mechanics to jednak algorytm mówi z wiekszym priorytetem
-        [WRESZCIE TO CO USER WYMUSIŁ] = -- to jest KURWA ŚWIĘTE!!! to nawet bym KURWA WZIĄŁ I JEBANE SKOPIOWAŁ DO TAKIEGO PLIKU BACKUPOWEGO KTÓRY ISTNIEJE I SIE AKTUALIZUJE PRAWDA Z KAZDYM STARTEM GRY Z TAJMSTAMPEM, do momentu jak nie stworzymy proper systemu storowania i odzyskiwania BEKAPUF
+        ["Base.ItemNumberOne"] = "Big Black Cock",
+        ["zygota"] = "Small white cock",-- self:getDisplayCategory() mam na myśli że self to "Base.ItemNumberOne" tylko to może zła sygnatura,
+        ["embrjon"] = "sex in the open, public place", --if (self:getDisplayCategory()) istnieje jako klucz w mapie no to tu storujemy te kategorie.. przyjkład - self:getDisplayCategory() == VehicleMaintentance, a wiec takie mapowanie isstnieje i zatem nadajemy mu prawda "Mechanics" ale chjcemy to mieć prawda w rekordach jako drugi wpis,
+        ["Mahomet"] = "Spermwalk",--czośtam czo wynikło z naaszego sotrotwania np. kurwa Base.TeddyBear -> "Plushie", albo mechanics -> waeapon , no to tutaj pomimo że mapa wskazala mechanics to jednak algorytm mówi z wiekszym priorytetem
+        ["podgrzybek"] = "handjob on the beach",-- to jest KURWA ŚWIĘTE!!! to nawet bym KURWA WZIĄŁ I JEBANE SKOPIOWAŁ DO TAKIEGO PLIKU BACKUPOWEGO KTÓRY ISTNIEJE I SIE AKTUALIZUJE PRAWDA Z KAZDYM STARTEM GRY Z TAJMSTAMPEM, do momentu jak nie stworzymy proper systemu storowania i odzyskiwania BEKAPUF
     }
-}
+
 
 -- CZY TY NIE JESTEŚ ZA GRUBA ABY TO ZROZUMIEĆ!?
